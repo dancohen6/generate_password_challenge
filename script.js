9f03006a9cac7c099241aa49a9d62dfb049f83ae
dancohen6 <dzcsix@gmail.com>
2023-06-07 15:58:14 -0400

    added final comments to JS

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// SET VARIABLES FOR ALL POSSIBLE CHARACTERS //
 var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
@@ -7,7 +8,7 @@ var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', 
 
 function getUserInput(){
     var amount = +prompt('Please provide an amount.');
-    // COMMENT
+    // INPUT MUST BE GREATER THAN OR EQUAL TO 8 AND LESS THAN OR EQUAL TO 128 //
     if (amount >=8 && amount <=128) {
     var includeLowercase = confirm('Click OK to include lowercase letters.')
     var includeUppercase = confirm('Click OK to include uppercase letters.')
@@ -22,6 +23,7 @@ function getUserInput(){
 }
 
 function generatePassword() {
+    // FUNCTION WITHIN A FUNCTION. [X] IS REFERENCING ARRAY IN THE getUserInput FUNCTION//
     var choices = getUserInput();
     var pass = '';
     var charAmount = choices[0];
@@ -47,9 +49,11 @@ function generatePassword() {
         allPossibleChars = allPossibleChars.concat(specialCharacters);
     }
 
+    // START FOR LOOP AT '0,' AND FOR AS LONG AS COUNTER IS LESS THAN NUMBER INPUT BY USER, CONTINUE TO RUN //
     for (var count = 0; count < charAmount; count++) {
         // GRAB RANDOM CHARACTER FROM allPossibleChars ARRAY //
         var randomNum = Math.random();
+        // RANDOMNUM MULTIPLIED BY ALLPOSSIBLECHARS, THEN .FLOOR WILL ROUND DOWN TO NEAREST WHOLE NUMBER //
         var randomIndex = Math.floor(randomNum * allPossibleChars.length);
         var randomChar = allPossibleChars[randomIndex];
 
